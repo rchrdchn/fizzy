43add9d0bec91d5117615b77e9101ce07e61c6d3
Jose Farias <jose@37signals.com>
2024-11-25 16:30:38 -0600

    Move combobox initializer to helper

--- app/helpers/combobox_helper.rb
+++ app/helpers/combobox_helper.rb
@@ -1,5 +1,6 @@
-class ActionView::Helpers::FormBuilder
-  def combobox(...)
+module ComboboxHelper
+  # `hw_combobox_tag` overrides HotwireCombobox's helper to provide default options.
+  def hw_combobox_tag(...)
     super do |combobox|
       combobox.customize_main_wrapper class: "btn"
       combobox.customize_input data: { autofocus_target: "element" }
